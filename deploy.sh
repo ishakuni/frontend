6f3ec13894a8a3083dacc9058e49c658b91e5f56
Pradip Caulagi <caulagi@gmail.com>
2018-10-28 20:27:20 +0100

    chore: create deployment in github using api (#12)
    * Add deploy script
    
    * Fix script to create and update deployment
    
    * move deploy steps inside script

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -x
+
+GITHUB_OWNER=${GITHUB_OWNER:-"ishakuni"}
+GITHUB_REPO=${GITHUB_REPO:-"frontend"}
+url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPO}/deployments"
+
+create_deployment() {
+    IFS='' read -r -d '' post_data <<-END_JSON
+    {
+        "ref": "create-github-deployment",
+        "payload": {
+            "deploy": "migrate"
+        },
+        "description": "test deploy"
+    }
+END_JSON
+
+    data=$(jq -n "$post_data")
+    response=$(curl --silent -H "Authorization: bearer $GITHUB_DEPLOYMENT_TOKEN" -X POST -d "$post_data" "$url")
+    statuses_url=$(jq --raw-output '.statuses_url' <<< $response)
+}
+
+do_deployment() {
+    yarn global add now
+    yarn run build
+    cp now.json build
+    cd build
+    now -d --token $NOW_TOKEN && now alias ishakuni.com --token $NOW_TOKEN
+}
+
+update_deployment_status() {
+    IFS='' read -r -d '' post_data <<-END_JSON
+    {
+        "state": "success",
+        "description": "deploy was successful"
+    }
+END_JSON
+    data=$(jq -n "$post_data")
+    response=$(curl --silent -H "Authorization: bearer $GITHUB_DEPLOYMENT_TOKEN" -X POST -d "$post_data" "$statuses_url")
+}
+
+create_deployment
+do_deployment
+update_deployment_status
