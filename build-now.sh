3b89da58ddcb63f58b41420613f556ba7e2465cf
Pradip Caulagi <caulagi@gmail.com>
2019-02-24 21:17:23 +0100

    Ignore output from extracting gcc

--- build-now.sh
+++ build-now.sh
@@ -7,7 +7,7 @@ basedir=`pwd`
 # install gcc (required for reasonml)
 cd /tmp
 wget https://dmmcy0pwk6bqi.cloudfront.net/gcc-4.8.5.tgz
-tar xvf gcc-4.8.5.tgz
+tar xvf gcc-4.8.5.tgz 2>/dev/null
 export PATH=/tmp/bin:$PATH
 export LD_LIBRARY_PATH=/tmp/lib:/tmp/lib64:$LD_LIBRARY_PATH
 export CPATH=/tmp/include
