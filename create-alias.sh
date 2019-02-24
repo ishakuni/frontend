6dd6d5ed0d2e5aa02f4d190a2188e5487fdfda06
Pradip Caulagi <caulagi@gmail.com>
2019-02-24 20:58:02 +0100

    Try creating an alias on commit to master

--- create-alias.sh
+++ create-alias.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 
-yarn global add now
+yarn global add now wait-for-now
+wait-for-now
 now alias ishakuni.com --token $ZEIT_API_TOKEN
 now alias www.ishakuni.com --token $ZEIT_API_TOKEN
