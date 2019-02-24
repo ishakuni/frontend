d8a8b80f133094783ce2778e36e60a9710861f28
Pradip Caulagi <caulagi@gmail.com>
2019-02-24 17:56:38 +0100

    Fix build when using v2 of now

--- build-now.sh
+++ build-now.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-echo "ok"
+set -xe
+
+yum install -y wget
+basedir=`pwd`
+
+cd /tmp
+wget https://dmmcy0pwk6bqi.cloudfront.net/gcc-4.8.5.tgz
+tar xvf gcc-4.8.5.tgz
+export PATH=/tmp/bin:$PATH
+export LD_LIBRARY_PATH=/tmp/lib:/tmp/lib64:$LD_LIBRARY_PATH
+export CPATH=/tmp/include
+export LIBRARY_PATH=/tmp/lib
+
+cd $basedir
 yarn
 yarn build
