f8ad16b15a7f080aa18a720badd4ff353dfab3aa
Pradip Caulagi <caulagi@gmail.com>
2019-02-24 20:01:42 +0100

    Upgrade now integration to v2 (#71)
    * Upgrade now integration to v2
    
    * Fix build when using v2 of now
    
    * Probably don't need to deploy to now from travis
    
    * Don't need dockerfile
    
    * Trigger build

---
+++ build-now.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -xe
+
+yum install -y wget
+basedir=`pwd`
+
+# install gcc (required for reasonml)
+cd /tmp
+wget https://dmmcy0pwk6bqi.cloudfront.net/gcc-4.8.5.tgz
+tar xvf gcc-4.8.5.tgz
+export PATH=/tmp/bin:$PATH
+export LD_LIBRARY_PATH=/tmp/lib:/tmp/lib64:$LD_LIBRARY_PATH
+export CPATH=/tmp/include
+export LIBRARY_PATH=/tmp/lib
+
+cd $basedir
+yarn
+yarn build
