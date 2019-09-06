c93136b22b567a2fb0d623e83e0697f8154b9cb4
Pradip Caulagi <caulagi@gmail.com>
2019-09-06 22:05:09 +0200

    Zeit now can automatically create the aliases on merge to master
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- create-deployment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xe
-
-url=`now --regions bru --name ishakuni-frontend --token $zeit_token`
-now alias ishakuni.com $url --token $zeit_token
-now alias www.ishakuni.com $url --token $zeit_token
