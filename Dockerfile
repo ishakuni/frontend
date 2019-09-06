d8bbcea59c627e0dab8d6bfe104b3c46bde42ee9
Pradip Caulagi <caulagi@gmail.com>
2019-09-06 12:25:08 +0200

    Upgrade zeit now integration to v2
    Signed-off-by: Pradip Caulagi <caulagi@gmail.com>

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:8 as base
-
-WORKDIR /usr/src
-
-COPY . .
-RUN yarn && yarn build
-
-FROM scratch
-COPY --from=base /usr/src/public /public
--- create-alias.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-
-yarn global add now wait-for-now
-wait-for-now
-now alias ishakuni.com --token $ZEIT_API_TOKEN
-now alias www.ishakuni.com --token $ZEIT_API_TOKEN
